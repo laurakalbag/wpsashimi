bb141f7a80f3a6454ad7c585526c8e9e9578e485
laurakalbag <github@laurakalbag.com>
2013-04-01 15:57:53 +0100

    Added copyright with site name and current year

--- wpsashimi/footer.php
+++ wpsashimi/footer.php
@@ -3,6 +3,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'sashimi' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'sashimi' ), 'WordPress' ); ?></a>
+			<small>Copyright &copy; <?php bloginfo( 'title' ); ?>, <?php echo date('Y')?>.</small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
