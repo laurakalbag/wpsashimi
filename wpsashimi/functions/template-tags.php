8e4ba526b2104eb6c23199c7827d2e04cce55f87
laurakalbag <github@laurakalbag.com>
2013-04-09 14:40:54 +0100

    Took odd use of <footer/> out of comment template

--- wpsashimi/functions/template-tags.php
+++ wpsashimi/functions/template-tags.php
@@ -73,25 +73,23 @@ function sashimi_comment( $comment, $args, $depth ) {
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<footer>
-				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 90 ); ?>
-					<?php printf( __( '%s <span class="says">says:</span>', 'sashimi' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em><?php _e( 'Your comment is awaiting moderation.', 'sashimi' ); ?></em>
-				<?php endif; ?>
+			<div class="comment-author vcard">
+				<?php echo get_avatar( $comment, 90 ); ?>
+				<?php printf( __( '%s <span class="says">says:</span>', 'sashimi' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+			</div><!-- .comment-author .vcard -->
+			<?php if ( $comment->comment_approved == '0' ) : ?>
+				<em><?php _e( 'Your comment is awaiting moderation.', 'sashimi' ); ?></em>
+			<?php endif; ?>
 
-				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'sashimi' ), get_comment_date(), get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'sashimi' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
-			</footer>
+			<div class="comment-meta commentmetadata">
+				<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>">
+				<?php
+					/* translators: 1: date, 2: time */
+					printf( __( '%1$s at %2$s', 'sashimi' ), get_comment_date(), get_comment_time() ); ?>
+				</time></a>
+				<?php edit_comment_link( __( '(Edit)', 'sashimi' ), ' ' );
+				?>
+			</div><!-- .comment-meta .commentmetadata -->
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
